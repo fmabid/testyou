b91efbd88da1d050a883f0a2f8acdce41d2ad4da
fmabid <fmaabid@gmail.com>
2020-06-21 12:29:33 +0600

    Added list in local state.

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,20 +1,55 @@
 <template>
   <div>
     <div>
-      <input type="text">
-      <button type="button">Submit</button>
+      <input type="text"
+             @keypress.enter="addToList"
+             v-model.trim="name">
+      <button type="button"
+              @click="addToList">Add name on list</button>
     </div>
 
     <router-link to="/">Back</router-link>
+
+    <div class="container-txt">
+      <ul>
+        <li :key="name" v-for="(name, index) in list">{{index+1}}. {{ name }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
-        name: "LoginView"
+        name: "LoginView",
+        data() {
+            return {
+                name: '',
+
+                list: [],
+            }
+        },
+        methods: {
+            addToList: function () {
+                this.list.push(this.name)
+                this.name = ''
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+  .container-txt{
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: block;
+    margin: 0 10px;
+    text-align: left;
+  }
 </style>
