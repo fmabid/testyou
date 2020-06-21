88d16936b03d600a4016378d778fd5a0fcbd14c3
fmabid <fmaabid@gmail.com>
2020-06-21 12:10:57 +0600

    - better structured, defined route in another dir.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import router from './routers'
+
 import App from './App.vue'
+
+Vue.use(router)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
