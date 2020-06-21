47285a14ab29147c6d54c9d75f9d900717f410c3
fmabid <fmaabid@gmail.com>
2020-06-21 12:39:58 +0600

    Initiated state manager

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import router from './routers'
+//import Vuex from 'vuex'
 
 import App from './App.vue'
+import router from './routers'
+import {store} from './stores'
 
 Vue.use(router)
 
@@ -9,5 +11,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
